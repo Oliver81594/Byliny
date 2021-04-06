18fe4498f0dd5d6a498b63828bab70a90211201d
Oliver81594 <olimail111@gmail.com>
2021-04-06 20:54:00 +0200

    Navigacia funguje a zapisal som prvu bilynu

--- app.js
+++ app.js
@@ -30,16 +30,31 @@ var kvet = {
 }
 
 var register = document.getElementById("register")
-var zoznam =   ["Alchemilka obyčajná", "Archyngelika lekárska", "Arnika horská",
+var zoznam =   ["Alchemilka obyčajná", "Archangelika lekárska", "Arnika horská",
                 "Baza čierna", "Bazalka pravá", "Bedrovník väčší", "Blen čierny", "Bolehlav škvrnitý", "Borievka obyčajná", "Borovica lesná", "Brečtan popínavý", "Breza previsnutá", "Brusnica čučoriedková", "Brusnica obyčajná", "Buk lesný",
                 "Cesnak cibulový", "Cesnak medvedí", "Cyklamén purpurový", "Čakanka obyčajná", "Čemerica čierna", "Čerešňa višňová", "Čiernohlávok obyčajný", 
                 "Ďatelina lúčna", "Deväťsil lekársky", "Divozel sápovitý", "Dráč obyčajný", "Drieň obyčajný", "Dub letný", "Dula podlhovastá", "Durman obyčajný", "Dúška materina",
+                "Fazuľa šarlátová", "Fenikel obyčajný", "Fialka trojfarebná", 
+                "Hadovník väčši", "Hloh obycajný", "Hluchavka biela", "Horec žltý", "Hrach siaty", "Chmeľ obyčajný", "Chren dedinský",
+                "Ihlica tŕnitá", "Imelo biele", 
+                "Jablčník obyčajný", "Jahoda obyčajná", "Jarabina vtáčia", "Jastrabina lekárska", "Javor mliečny", "Jedľa biela", "Jelša lepkavá", 
+                "Kapsička pastierska", "Karbinec európsky", "Komonica lekárska", "Konopa siata", "Konvalinka vonavá", "Kôpor voňavý", "Koriander siaty", "Kosatec nemecký", "Kostihoj lekársky", "Kotvičník zemný", "Krasovlas bezbyľový", "Krušina jelšová", "Krvavec lekársky", "Kukurica siata", "Kýchavica biela",
+                "Ľan siaty", "Lastovičník väčší", "Lekno biele", "Levanduľa úzkolistá", "Lieska obyčajná", "Ligurček lekársky", "Lipa malolistá", "Lipkavec marinkový", "Lopúch väčší", "Ľubovník bodkovaný", "Ľuľkovec zlomocný", "Ľuľok sladkohorký", "Lykovec jedovatý",
                 ""]
+
+
+function priradID(){
+    id = this.id
+    localStorage.setItem("PrisielSomZ", id)
+}
 
 for (i = 0; i < zoznam.length; i++){
     meno = document.createElement("a")
     meno.className = "list-group-item list-group-item-action"
-    meno.href = "bylina.html"
+    meno.href = "Bylina/bylina.html"
     meno.textContent = zoznam[i]
+    meno.setAttribute("id", zoznam[i])
+    meno.addEventListener("click", priradID)
     register.append(meno)
 }
+
---
+++ register.js
@@ -0,0 +1,25 @@
+var alchemilka = {
+    zaklad:{
+        nazov:"Alchemilka obyčajná",
+        latinsky:"Alchemilla vulgaris",
+        strana:"10"
+    },
+    opis:{
+        popis:"Alchemilka obyčajná je trváca bylina pomerne premenlivého vzhľadu, pretože ide o agregátny druh, ktorý zahŕňa celý rad poddruhov a množstvo krížencov. Rastlina má drevnatý podzemok, z ktorého vyrastajú početné listy a kvitnúce stonky. Listy bývajú na rube a na stopkách chlpaté, na líci sú holé a v ich strede sa značnú časť doobedia často udržiavajú kvapky rosy. Súkvetie sa skladá z drobných žltozelených kvetov. Kvitne od mája do septembra.",
+        vyskyt:"Alchemilka sa vyskytuje najmä v miernom podnebnom pásme Európy a západnej ázie, v severnej Afrike a vo východnej časti Severnej Ameriky. U nás rastie veľmi hojne od nížin až po horské oblasti na lúkach a pastvinách, pri prameniskách, vo svetlých vlhších lesoch, popri potokoch a cestách. Najľhšie ju nájdeme na horských lúkach."
+    },
+    zber:{
+        mesiace:[5,6,7,8,9],
+        casti:["listy", "kvitnúca vňať"],
+        text:"Na liečebné účely sa od mája do septembra zbiera kvitnúca vňať a listy. Sušia sa prirodzene v tieni, pri umelom sušení by teplota nemala presiahnuť 40℃. Kvalitná droga si uchováva zelenú farbu a nedrobí sa."
+    },
+    liecba:{
+        liecitelstvo:"Alchemilka je jeden z najlepších prostriedkov na gynekologické problémy. Tlmí nadmerné krvácanie a bolestivosť pri menštruácii. Pitie čaju sa odporúča tehotným ženám ako prevencia potratu a na optimálny vývoj plodu aj an ľahší pôrod. Zmierňuje klimakterické ťažkosti, lieči biely výtok. Zvonka sa používa na výplachy pri gynekologických zápaloch a na hemoroidy, pretože potláča svrbenie slizníc. Pomáha aj pri kŕčových bolestiach svalstva, pri krvácaní z nosa alebo ako kloktadl, vo forme klystíru sa užíva napríklad pri zápale hrubého čreva. Odvar či šťava vytlačená z čerstvej vňate sa používa na umývanie rán, podliatin a vredov.",
+        fandly:"Ak si ženy budú umývať lono vodou, v ktorej sa varila alchemilka stále budú mať také malé prsníky, ako keď boli panny. Rovnaký účinok bude mať aj šatka nameočená do tejto vody a priložená na prsníky. Proti padajúcej chorobe(epilepsii) zmiešaj rovnaké množstvo alechemilky a plavúňa, hoď ho na žeravé uhlie a dymťahaj do seba ústami i nosom.",
+    },
+    recept:{
+        nazov:"Odvar z alchemilky",
+        ingrediencie:["3 kávové lyžičky pokrájanej suchej vňate alchemilky", "1/4 l vody"],
+        priprava:"Vňať zalejeme studenou vodou, uvedieme do varu a odstavíme. Po 15 minútach precedíme a vypijeme. Odporúčaná dávka je jedna šálka dva razy denne. Čaj môžeme pripraviť aj z čerstvej vňate, pijeme ho nesladený. Pri silnej menštruácii sa odporúča piť rovnako pripravenú čajovú zmes z alchemilky a rebríčka v pomere 1:1."
+    }
+}
