4bb1694f615277c2f40c86a0e05fa8dc349d41a8
Oliver Kurnava <55894461+Oliver81594@users.noreply.github.com>
2022-06-25 10:24:26 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,19 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-
-workbox.routing.registerRoute(
-    ({request}) => request.destination === 'image',
-    new workbox.strategies.NetworkFirst()
-);
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('byliny_cache').then(function(cache) {
+      return cache.addAll([
+        // your list of cache keys to store in cache
+        'index.html',
+        'register.js',
+        'manifest.json',
+        'style.css',
+        'bylina/bylina.html',
+        'bylina/bylina.js',
+        'bylina/style.css',
+        'js/app.js',
+        'hladanie.js'
+        // etc.
+      ])
+    })
+  );
+});
